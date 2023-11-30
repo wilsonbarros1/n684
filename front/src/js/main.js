a63d0e2c2af2dc22d2305805d0fa3e81587a4d56
Daniel Viana <danzprogdanz@gmail.com>
2023-11-30 05:06:27 -0300

    tabelas populadas

--- front/src/js/main.js
+++ front/src/js/main.js
@@ -1,28 +1,129 @@
 import axios from "/@fs/C:/Users/Usuário/Desktop/ADS_3/webdev/front/node_modules/.vite/deps/axios.js?v=cdba558c";
-  
-        const baseUrl = `http://3.23.103.69:3030/api/curso`;
-        
 
-        const getCourses = async () => {
-  try {
-    const response = await axios.get(baseUrl);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching courses:', error);
-    throw error;
-  }
-};
+if (window.location.pathname === '/alunos') {
+  const baseUrlAlunos = 'http://3.23.103.69:3030/api/aluno';
 
-// Use an async function to handle the Promise returned by getCourses
-const fetchAndLogCourses = async () => {
-  try {
-    const arr = await getCourses();
-    console.log(arr);
-  } catch (error) {
-    // Handle errors if necessary
-    console.error('Error:', error);
-  }
-};
+  const getAlunos = async () => {
+    try {
+      const response = await axios.get(baseUrlAlunos);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching students:', error);
+      throw error;
+    }
+  };
 
-// Call the async function
-fetchAndLogCourses();
+  const fetchAndPopulateAlunos = async () => {
+    try {
+      const alunosArray = await getAlunos();
+
+      // Get the tbody element
+      const tbody = document.getElementById('alunosTableBody');
+
+      // Clear any existing rows in the tbody
+      tbody.innerHTML = '';
+
+      // Loop through the alunosArray and create a row for each aluno
+      alunosArray.forEach(aluno => {
+        const row = document.createElement('tr');
+
+        // Create cells for each column in the table
+        const idCell = document.createElement('td');
+        idCell.textContent = aluno._id;
+
+        const nomeCell = document.createElement('td');
+        nomeCell.textContent = aluno.nome;
+
+        const idadeCell = document.createElement('td');
+        idadeCell.textContent = aluno.idade;
+
+        const cursoCell = document.createElement('td');
+        cursoCell.textContent = aluno.curso;
+
+        const emailCell = document.createElement('td');
+        emailCell.textContent = aluno.email;
+
+        // Add action buttons or any other columns as needed
+        const actionsCell = document.createElement('td');
+        actionsCell.textContent = 'Actions'; // Replace with your actual action buttons
+
+        // Append cells to the row
+        row.appendChild(idCell);
+        row.appendChild(nomeCell);
+        row.appendChild(idadeCell);
+        row.appendChild(cursoCell);
+        row.appendChild(emailCell);
+        row.appendChild(actionsCell);
+
+        // Append the row to the tbody
+        tbody.appendChild(row);
+      });
+
+      console.log('Alunos table populated successfully');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  fetchAndPopulateAlunos();
+}
+
+if (window.location.pathname === '/cursos') {
+  const baseUrlCursos = 'http://3.23.103.69:3030/api/curso';
+
+  const getCourses = async () => {
+    try {
+      const response = await axios.get(baseUrlCursos);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching courses:', error);
+      throw error;
+    }
+  };
+
+  const fetchAndPopulateCourses = async () => {
+    try {
+      const coursesArray = await getCourses();
+
+      // Get the tbody element
+      const tbody = document.getElementById('coursesTableBody');
+
+      // Clear any existing rows in the tbody
+      tbody.innerHTML = '';
+
+      // Loop through the coursesArray and create a row for each course
+      coursesArray.forEach(course => {
+        const row = document.createElement('tr');
+
+        // Create cells for each column in the table
+        const idCell = document.createElement('td');
+        idCell.textContent = course._id;
+
+        const cursoCell = document.createElement('td');
+        cursoCell.textContent = course.descricao;
+
+        const instituicaoCell = document.createElement('td');
+        instituicaoCell.textContent = course.nomeInstituicao;
+
+        // Add action buttons or any other columns as needed
+        const actionsCell = document.createElement('td');
+        actionsCell.textContent = 'Actions'; // Replace with your actual action buttons
+
+        // Append cells to the row
+        row.appendChild(idCell);
+        row.appendChild(cursoCell);
+        row.appendChild(instituicaoCell);
+        row.appendChild(actionsCell);
+
+        // Append the row to the tbody
+        tbody.appendChild(row);
+      });
+
+      console.log('Courses table populated successfully');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  fetchAndPopulateCourses();
+}
--- front/src/router/router.js
+++ front/src/router/router.js
@@ -1,9 +1,4 @@
-const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-}
-
+// Router configuration
 const routerConfig = {
     baseElementId: "main-page",
     routes: {
@@ -16,10 +11,8 @@ const routerConfig = {
     },
 };
 
-const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routerConfig.routes[path] || routerConfig.routes[404];
-
+// Function to fetch content from the server
+const fetchContent = async (route) => {
     try {
         const response = await fetch(route);
 
@@ -27,23 +20,79 @@ const handleLocation = async () => {
             throw new Error(`Failed to fetch: ${response.status} ${response.statusText}`);
         }
 
-        const html = await response.text();
-        const targetElement = document.getElementById(routerConfig.baseElementId);
-        
-        if (targetElement) {
-            targetElement.innerHTML = html;
-        } else {
-            console.error(`Target element with ID '${routerConfig.baseElementId}' not found.`);
-        }
+        return await response.text();
     } catch (error) {
-        console.error(`Error during route handling: ${error.message}`);
-        // Handle errors (e.g., show a generic error page)
+        console.error('Error fetching content:', error);
+        throw error;
     }
-}
+};
 
+// Function to update the DOM with fetched content
+const updateDOM = (html, targetElementId) => {
+    const targetElement = document.getElementById(targetElementId);
+
+    if (targetElement) {
+        targetElement.innerHTML = html;
+    } else {
+        console.error(`Target element with ID '${targetElementId}' not found.`);
+    }
+};
+
+// Placeholder functions for loading indicators
+const showLoadingIndicator = () => {
+    // Display a loading indicator (implement based on your UI)
+};
+
+const hideLoadingIndicator = () => {
+    // Hide the loading indicator (implement based on your UI)
+};
+
+// Function to handle errors during route handling
+const handleErrors = (error) => {
+    // Handle errors (e.g., show a generic error page)
+    console.error('Error during route handling:', error);
+    // Optionally redirect to a generic error page
+    window.location.href = '/pages/error.html';
+};
+
+// Function to handle location changes
+const handleLocation = async () => {
+    showLoadingIndicator();
+
+    const path = window.location.pathname;
+    const route = routerConfig.routes[path] || routerConfig.routes[404];
+
+    try {
+        const html = await fetchContent(route);
+        updateDOM(html, routerConfig.baseElementId);
+    } catch (error) {
+        handleErrors(error);
+    } finally {
+        hideLoadingIndicator();
+    }
+};
+
+// Event listener for clicks on anchor tags using event delegation
+document.body.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+        event.preventDefault();
+        window.history.pushState({}, '', event.target.href);
+        handleLocation();
+    }
+});
+
+// Event listener for popstate events (e.g., back/forward buttons)
 window.onpopstate = handleLocation;
-window.route = route;
 
+// Expose the route function to the global scope
+window.route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, '', event.target.href);
+    handleLocation();
+};
+
+// Initial handling of location to set up the initial state
 handleLocation();
 
 /////////////////
